a9ec9ac16f63231a9a1aa0eda25c1b28e10beb73
Thomas Webel <Thomaswebel@gmail.com>
2021-04-20 17:51:12 -0400

    Filled in user Enitiy / added relationship mapping test

--- JPAVoyager/src/main/java/com/skilldistillery/country/entities/User.java
+++ JPAVoyager/src/main/java/com/skilldistillery/country/entities/User.java
@@ -1,9 +1,20 @@
 package com.skilldistillery.country.entities;
 
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.util.List;
+
+import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.OneToMany;
+
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.UpdateTimestamp;
+
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 
 @Entity
 public class User {
@@ -14,12 +25,51 @@ public class User {
 
 	private String username;
 	
+	private String email;
+	
+	private String password;
+	
+	@Column(name="first_name")
+	private String firstName;
+	
+	@Column(name="middle_name")
+	private String middleName;
+	
+	@Column(name="last_name")
+	private String lastName;
+	
+	private String suffix;
+	
+	private LocalDate dob;
+	
+	private Boolean enable;
+
+	@CreationTimestamp
+	@Column(name="create_date")
+	private LocalDateTime createDate;
+	
+	@UpdateTimestamp
+	@Column(name="update_date")
+	private LocalDateTime updateDate;
+	
+//	@OneToMany(mappedBy = "user")
+//	@JsonIgnoreProperties(value="user")
+//	private List<Trip> trips;
+//	
+//	@OneToMany(mappedBy = "user")
+//	@JsonIgnoreProperties(value="user")
+//	private List<Comment> comments;
 	
 	
-//////// methods
+// Methods
+	
+	// Constructors
+	
 	public User() {
 	}
-
+	
+	// Get / Set
+	
 	public int getId() {
 		return id;
 	}
@@ -36,6 +86,86 @@ public class User {
 		this.username = username;
 	}
 
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	public String getPassword() {
+		return password;
+	}
+
+	public void setPassword(String password) {
+		this.password = password;
+	}
+
+	public String getFirstName() {
+		return firstName;
+	}
+
+	public void setFirstName(String firstName) {
+		this.firstName = firstName;
+	}
+
+	public String getMiddleName() {
+		return middleName;
+	}
+
+	public void setMiddleName(String middleName) {
+		this.middleName = middleName;
+	}
+
+	public String getLastName() {
+		return lastName;
+	}
+
+	public void setLastName(String lastName) {
+		this.lastName = lastName;
+	}
+
+	public String getSuffix() {
+		return suffix;
+	}
+
+	public void setSuffix(String suffix) {
+		this.suffix = suffix;
+	}
+
+	public LocalDate getDob() {
+		return dob;
+	}
+
+	public void setDob(LocalDate dob) {
+		this.dob = dob;
+	}
+
+	public Boolean getEnable() {
+		return enable;
+	}
+
+	public void setEnable(Boolean enable) {
+		this.enable = enable;
+	}
+
+	public LocalDateTime getCreateDate() {
+		return createDate;
+	}
+
+	public void setCreateDate(LocalDateTime createDate) {
+		this.createDate = createDate;
+	}
+
+	public LocalDateTime getUpdateDate() {
+		return updateDate;
+	}
+
+	public void setUpdateDate(LocalDateTime updateDate) {
+		this.updateDate = updateDate;
+	}
+	// Hash / Equals
 	@Override
 	public int hashCode() {
 		final int prime = 31;
@@ -57,10 +187,17 @@ public class User {
 			return false;
 		return true;
 	}
-
+	
+	// toString
 	@Override
 	public String toString() {
-		return "User [id=" + id + ", username=" + username + "]";
+		StringBuilder builder = new StringBuilder();
+		builder.append("User [id=").append(id).append(", username=").append(username).append(", email=").append(email)
+				.append(", password=").append(password).append(", firstName=").append(firstName).append(", middleName=")
+				.append(middleName).append(", lastName=").append(lastName).append(", suffix=").append(suffix)
+				.append(", dob=").append(dob).append(", enable=").append(enable).append(", createDate=")
+				.append(createDate).append(", updateDate=").append(updateDate).append("]");
+		return builder.toString();
 	}
 
 
