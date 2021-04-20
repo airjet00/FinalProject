946bb84cd861546e039e2ab68e66002673df6e1a
Thomas Webel <Thomaswebel@gmail.com>
2021-04-20 15:48:32 -0400

    Added @crossOrigin to user controller

--- voyager/src/main/java/com/skilldistillery/country/controllers/UserController.java
+++ voyager/src/main/java/com/skilldistillery/country/controllers/UserController.java
@@ -1,14 +1,16 @@
 package com.skilldistillery.country.controllers;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.skilldistillery.country.services.UserService;
 
-@RequestMapping("api")
 @RestController
+@RequestMapping("api")
+@CrossOrigin({"*", "http://localhost:4290"})
 public class UserController {
 
 	@Autowired 
