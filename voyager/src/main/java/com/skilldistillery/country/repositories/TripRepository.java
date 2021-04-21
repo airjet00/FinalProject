6dbaefbe863cea2444f346920c99cf3f8e5e4a2b
Your Name <you@example.com>
2021-04-20 18:13:14 -0600

    completed trip service implementation

--- voyager/src/main/java/com/skilldistillery/country/repositories/TripRepository.java
+++ voyager/src/main/java/com/skilldistillery/country/repositories/TripRepository.java
@@ -1,9 +1,12 @@
 package com.skilldistillery.country.repositories;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 
-import com.skilldistillery.country.entities.AdviceType;
+import com.skilldistillery.country.entities.Trip;
 
-public interface TripRepository extends JpaRepository<AdviceType, Integer> {
-
+public interface TripRepository extends JpaRepository<Trip, Integer> {
+	List<Trip> findByUser_username(String username);
+	Trip findByUser_usernameAndId(String username, int id);
 }
