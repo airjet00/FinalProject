9984b8271d51682873bfb3f679c63b6f5072733c
Thomas Webel <Thomaswebel@gmail.com>
2021-04-20 20:14:59 -0400

    Stubbed User Service

--- voyager/src/main/java/com/skilldistillery/country/services/UserService.java
+++ voyager/src/main/java/com/skilldistillery/country/services/UserService.java
@@ -1,5 +1,19 @@
 package com.skilldistillery.country.services;
 
+import java.util.List;
+
+import com.skilldistillery.country.entities.User;
+
 public interface UserService {
+
+	public List<User> index(String username);
+
+	public User show(String username, int uid);
+
+	public User create(String username, User user);
+
+	public User update(String username, int uid, User user);
+
+	public boolean destroy(String username, int uid);
 
 }
--- voyager/src/main/java/com/skilldistillery/country/services/UserServiceImpl.java
+++ voyager/src/main/java/com/skilldistillery/country/services/UserServiceImpl.java
@@ -1,8 +1,11 @@
 package com.skilldistillery.country.services;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import com.skilldistillery.country.entities.User;
 import com.skilldistillery.country.repositories.UserRepository;
 
 @Service
@@ -10,4 +13,34 @@ public class UserServiceImpl implements UserService {
 
 	@Autowired 
 	private UserRepository userRepo;
+
+	@Override
+	public List<User> index(String username) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public User show(String username, int uid) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public User create(String username, User user) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public User update(String username, int uid, User user) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public boolean destroy(String username, int uid) {
+		// TODO Auto-generated method stub
+		return false;
+	}
 }
