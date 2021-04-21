937890ba348ae8d0787523acef122de1a649b6c8
Eric Paulson <ericpaulson@Erics-MacBook-Pro.local>
2021-04-21 10:12:08 -0600

    create admin test user in DB; update CommentController

---
+++ voyager/src/main/java/com/skilldistillery/country/controllers/CommentController.java
@@ -0,0 +1,28 @@
+package com.skilldistillery.country.controllers;
+
+import java.security.Principal;
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.skilldistillery.country.entities.Comment;
+import com.skilldistillery.country.services.CommentService;
+
+@CrossOrigin({ "*", "http://localhost:4290" })
+@RequestMapping("api")
+@RestController
+public class CommentController {
+
+	@Autowired
+	private CommentService commentServ;
+
+	@GetMapping("comments")
+	public List<Comment> index(Principal principal) {
+		return commentServ.index(principal.getName());
+	}
+
+}
