54c8daf6e1b31d635edc3dbc56b5cebc73eac7f2
Eric Paulson <ericpaulson@Erics-MacBook-Pro.local>
2021-04-21 17:10:49 -0600

    link CommentService to Component with basic READ and CREATE functionality

--- ngVoyagerHB/src/app/app-routing.module.ts
+++ ngVoyagerHB/src/app/app-routing.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { CommentListComponent } from './components/comment-list/comment-list.component';
 import { CountryListComponent } from './components/country-list/country-list.component';
 import { HomeComponent } from './components/home/home.component';
 import { LoginComponent } from './components/login/login.component';
@@ -12,6 +13,7 @@ const routes: Routes = [  { path: '', pathMatch: 'full', redirectTo: 'home' },
 { path: 'login', component: LoginComponent },
 { path: 'countries', component: CountryListComponent },
 { path: 'countries/:cid', component: CountryListComponent },
+{ path: 'comment-list', component: CommentListComponent },
 { path: '**', component: NotFoundComponent }
 ];
 
