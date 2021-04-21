ae73bdab1c449f4334ff0751b8423c2004c27254
Eric Paulson <ericpaulson@Erics-MacBook-Pro.local>
2021-04-20 18:36:59 -0600

    create CommentService

--- voyager/src/main/java/com/skilldistillery/country/repositories/CommentRepository.java
+++ voyager/src/main/java/com/skilldistillery/country/repositories/CommentRepository.java
@@ -1,9 +1,13 @@
 package com.skilldistillery.country.repositories;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.skilldistillery.country.entities.Comment;
 
 public interface CommentRepository extends JpaRepository<Comment, Integer> {
 
+	List<Comment> findByUser_Username(String username);
+	
 }
