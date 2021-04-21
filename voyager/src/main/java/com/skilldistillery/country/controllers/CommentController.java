5dccca6ebf7024753e850b7c52ef874c1913d9c2
Eric Paulson <ericpaulson@Erics-MacBook-Pro.local>
2021-04-21 11:49:27 -0600

    implement POST api/comments endpoint

--- voyager/src/main/java/com/skilldistillery/country/controllers/CommentController.java
+++ voyager/src/main/java/com/skilldistillery/country/controllers/CommentController.java
@@ -9,6 +9,8 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -41,82 +43,27 @@ public class CommentController {
 			comment = commentServ.show(principal.getName(), cid);
 			if (comment != null) {
 				resp.setStatus(200);
-			}
-			else {
+			} else {
 				resp.setStatus(404);
 			}
 		}
 		return comment;
 	}
 
-	/*
-	 * 
-	 * 
-	 * @GetMapping("articles/{id}") public JournalArticle findById(@PathVariable
-	 * Integer id, HttpServletResponse resp) { JournalArticle ja =
-	 * jaServ.findById(id); if (ja == null) { resp.setStatus(404); } return ja; }
-	 * 
-	 * @GetMapping("articles/search/{searchTerm}") public List<JournalArticle>
-	 * search(@PathVariable String searchTerm) { return jaServ.search(searchTerm); }
-	 * 
-	 * 
-	 * @GetMapping("articles/aggregates/count") public long count() { return
-	 * jaServ.count(); }
-	 * 
-	 * @PostMapping("articles") public JournalArticle create(@RequestBody
-	 * PayloadUtility pcu, HttpServletRequest req, HttpServletResponse resp) {
-	 * JournalArticle ja = null; Author author = null;
-	 * 
-	 * try { ja = pcu.getJa(); author = pcu.getAuthor();
-	 * System.out.println("author: " + author); System.out.println("ja: " + ja);
-	 * 
-	 * author = authorServ.create(author); ja = jaServ.create(ja);
-	 * 
-	 * System.out.println("author: " + author); System.out.println("ja: " + ja);
-	 * 
-	 * if (ja != null && author != null) { jaServ.addAuthor(ja.getId(),
-	 * author.getId()); resp.setStatus(201); }
-	 * 
-	 * if (ja == null || author == null) { resp.setStatus(400); }
-	 * 
-	 * StringBuffer url = req.getRequestURL(); url.append("/").append(ja.getId());
-	 * resp.setHeader("Location", url.toString()); } catch (Exception e) {
-	 * System.err.println(e); e.printStackTrace(); resp.setStatus(400); ja = null; }
-	 * return ja; }
-	 * 
-	 * @PutMapping("articles/{id}") public JournalArticle update(@PathVariable
-	 * Integer id, @RequestBody JournalArticle ja, HttpServletResponse resp) { try {
-	 * ja = jaServ.update(id, ja); if (ja == null) { resp.setStatus(404); } } catch
-	 * (Exception e) { System.err.println(e); resp.setStatus(400); ja = null; }
-	 * return ja; }
-	 * 
-	 * @PutMapping("articles/{jaId}/authors/{authorId}") public JournalArticle
-	 * addAuthor(@PathVariable Integer jaId, @PathVariable Integer authorId,
-	 * HttpServletResponse resp) { JournalArticle ja = null;
-	 * 
-	 * try { ja = jaServ.findById(jaId);
-	 * 
-	 * jaServ.addAuthor(jaId, authorId);
-	 * 
-	 * if (ja == null) { resp.setStatus(404); } } catch (Exception e) {
-	 * System.err.println(e); resp.setStatus(400); ja = null; } return ja; }
-	 * 
-	 * // @PostMapping("articles/authors") // public JournalArticle
-	 * createArticleWithAuthor(@RequestBody JournalArticle ja, @RequestBody Author
-	 * author, // HttpServletResponse resp) { // try { // author =
-	 * authorServ.create(author); // ja = jaServ.create(ja); // if (ja != null &&
-	 * author != null) jaServ.addAuthor(ja.getId(), author.getId()); // // if (ja ==
-	 * null || author == null) { // resp.setStatus(400); // } // // } catch
-	 * (Exception e) { // System.err.println(e); // resp.setStatus(400); // ja =
-	 * null; // } // // return ja; // }
-	 * 
-	 * @DeleteMapping("articles/{id}") public boolean delete(@PathVariable Integer
-	 * id, HttpServletResponse resp) { boolean victory = false; try { if
-	 * (jaServ.delete(id)) { resp.setStatus(204); victory = true; } else
-	 * resp.setStatus(404); } catch (Exception e) { System.err.println(e);
-	 * resp.setStatus(400); } return victory; }
-	 * 
-	 * 
-	 * 
-	 */
+	@PostMapping("comments")
+	public Comment create(Principal principal, @RequestBody Comment comment, HttpServletResponse resp) {
+		try {
+			comment = commentServ.create(principal.getName(), comment);
+			resp.setStatus(201);
+		} catch (Exception e) {
+			System.err.println(e);
+			e.printStackTrace();
+			resp.setStatus(400);
+			comment = null;
+		}
+		return comment;
+	}
+	 
+	
+	
 }
