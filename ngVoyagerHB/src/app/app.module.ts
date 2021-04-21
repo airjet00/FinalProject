e0de8f48e96efdfd53e4b98d1e48a21373ed0dd1
Thomas Webel <Thomaswebel@gmail.com>
2021-04-21 15:27:56 -0400

    Stubbed initial Components in ngVoyagerHB

--- ngVoyagerHB/src/app/app.module.ts
+++ ngVoyagerHB/src/app/app.module.ts
@@ -3,10 +3,32 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NavigationComponent } from './components/navigation/navigation.component';
+import { FooterComponent } from './components/footer/footer.component';
+import { RegisterComponent } from './components/register/register.component';
+import { LoginComponent } from './components/login/login.component';
+import { LogoutComponent } from './components/logout/logout.component';
+import { CountryListComponent } from './components/country-list/country-list.component';
+import { SearchComponent } from './components/search/search.component';
+import { TripListComponent } from './components/trip-list/trip-list.component';
+import { UserListComponent } from './components/user-list/user-list.component';
+import { CommentListComponent } from './components/comment-list/comment-list.component';
+import { PictureListComponent } from './components/picture-list/picture-list.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavigationComponent,
+    FooterComponent,
+    RegisterComponent,
+    LoginComponent,
+    LogoutComponent,
+    CountryListComponent,
+    SearchComponent,
+    TripListComponent,
+    UserListComponent,
+    CommentListComponent,
+    PictureListComponent
   ],
   imports: [
     BrowserModule,
