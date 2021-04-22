b2a927366e3b2e8b90c706816acb7eb674351e75
Thomas Webel <Thomaswebel@gmail.com>
2021-04-22 12:02:47 -0400

    Added user info to local storage

--- voyager/src/main/java/com/skilldistillery/country/services/UserService.java
+++ voyager/src/main/java/com/skilldistillery/country/services/UserService.java
@@ -9,6 +9,8 @@ public interface UserService {
 	public List<User> index(String username);
 
 	public User show(String username, int uid);
+	
+	public User showByUserName(String username);
 
 	public User create(String username, User user);
 
