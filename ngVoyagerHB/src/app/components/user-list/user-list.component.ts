2021941b0618e632811aa3613b0c1ee0f7c6d399
erikskell <erikskell@gmail.com>
2021-04-22 09:56:47 -0500

    added load method to user component.ts

--- ngVoyagerHB/src/app/components/user-list/user-list.component.ts
+++ ngVoyagerHB/src/app/components/user-list/user-list.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { User } from 'src/app/models/user';
+import { UserService } from 'src/app/services/user.service';
 
 @Component({
   selector: 'app-user-list',
@@ -7,9 +9,29 @@ import { Component, OnInit } from '@angular/core';
 })
 export class UserListComponent implements OnInit {
 
-  constructor() { }
+  users: User[] = [];
+
+  constructor(
+    private userServ: UserService
+  ) { }
 
   ngOnInit(): void {
+    this.loadUsers();
   }
 
+
+
+  loadUsers(){
+    this.userServ.index().subscribe(
+      data => {
+        this.users = data;
+        return data;
+      },
+      fail => {
+        console.error("Error loading users " + fail);
+        console.error(fail);
+
+      });
+      return null;
+  }
 }
