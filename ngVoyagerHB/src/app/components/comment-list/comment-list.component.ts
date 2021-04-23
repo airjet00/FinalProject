e2e1186973c2777df057a2f5f85f394f9ece03b5
Eric Paulson <ericpaulson@Erics-MacBook-Pro.local>
2021-04-23 16:45:43 -0600

    refactor comment-list and country-list components

--- ngVoyagerHB/src/app/components/comment-list/comment-list.component.ts
+++ ngVoyagerHB/src/app/components/comment-list/comment-list.component.ts
@@ -4,6 +4,9 @@ import { Comment } from 'src/app/models/comment';
 import { Country } from 'src/app/models/country';
 import { ActivatedRoute, Router } from '@angular/router';
 import { AuthService } from 'src/app/services/auth.service';
+import { CountryService } from 'src/app/services/country.service';
+import { AdviceType } from 'src/app/models/advice-type';
+import { Picture } from 'src/app/models/picture';
 
 @Component({
   selector: 'app-comment-list',
@@ -16,26 +19,52 @@ export class CommentListComponent implements OnInit {
   countryId: number;
   country: Country;
 
-  comments: Comment[] = null;
+  // comments: Comment[] = null;
   showComment: Comment;
 
-  createComment: Comment = new Comment();
+  // createComment: Comment = new Comment();
   createCommentResult: Comment;
 
   updateComment: Comment = new Comment();
 
   commentForDeletion: Comment = null;
+
+
+  ///////////////////
+
+  role: string = null;
+  username: string = null;
+  countries: Country[] = null;
+  selected: Country = null;
+  newCountry: Country = new Country();
+  editCountry: Country = null;
+  keyword: String = null;
+  pictures: Picture[] = null;
+  advice: AdviceType[] = null;
+  comments: Comment[] = null;
+  commentEdit: Comment = null;
+  commentIndex: number = null;
+  responseEdit: Comment = null;
+  responseIndex: number = null;
+  createComment: Comment = new Comment();
+
   //////// init:
-  constructor(private router: Router, private authService: AuthService, private commentServ: CommentService, private route: ActivatedRoute) { }
+  constructor(private router: Router, private authService: AuthService, private commentServ: CommentService, private route: ActivatedRoute, private countryServ: CountryService) { }
 
   ngOnInit(): void {
     this.countryId = +this.route.snapshot.paramMap.get('countryId');
-    this.loadComments();
 
-    this.createComment = new Comment();
+    this.username = localStorage.getItem("username");
+    console.warn(this.username);
+
+
+    // this.loadComments();
+    // this.createComment = new Comment();
     // this.country = new Country();
     // this.country.id = this.countryId
     // this.createComment.country = this.country;
+    // this.role = localStorage.getItem("userRole");
+
 
   }
 
@@ -54,7 +83,7 @@ export class CommentListComponent implements OnInit {
 
   prepareUpdate(comment): void {
     console.warn("** in component, prepareUpdate()");
-    console.warn("** comment.id = "+ comment.id);
+    console.warn("** comment.id = " + comment.id);
 
 
     this.updateComment = comment;
@@ -62,7 +91,7 @@ export class CommentListComponent implements OnInit {
 
   confirmUpdate() {
     console.warn("** in component, confirmUpdate()");
-    console.warn("** this.updateComment.id = "+ this.updateComment.id);
+    console.warn("** this.updateComment.id = " + this.updateComment.id);
     // console.warn("***************************\nthis.updateComment.user is" + this.updateComment.user.id + " " + this.updateComment.user.firstName);
 
     this.commentServ.update(this.updateComment, this.countryId).subscribe(
@@ -90,7 +119,7 @@ export class CommentListComponent implements OnInit {
   }
 
 
-  delete(comment : Comment) : void {
+  delete(comment: Comment): void {
     this.commentForDeletion = comment;
     this.commentServ.delete(this.countryId, this.commentForDeletion.id).subscribe(
       dataReceived => {
@@ -100,6 +129,119 @@ export class CommentListComponent implements OnInit {
         console.error(failure);
       });
 
+  }
+
+
+  /////////////////////////////
+
+
+  loadCountries() {
+    this.countryServ.index().subscribe(
+      data => {
+        this.countries = data;
+      },
+      err => console.error('loadCountries got an error: ' + err)
+    )
+  }
+
+  selectCountry(country: Country) {
+    this.selected = country;
+    this.router.navigateByUrl('countries/' + country.id)
+  }
+
+  showCountry(cid) {
+    this.countryServ.show(cid).subscribe(
+      data => {
+        this.selected = data;
+        this.pictures = data["pictures"]
+        this.advice = data["adviceTypes"]
+        // this.loadComments(cid);
+      },
+      err => console.error('showCountries got an error: ' + err)
+    )
+  }
+
+  loadComments1(cid) {
+
+    this.commentServ.index(cid).subscribe(
+      data => {
+        this.comments = data;
+      },
+      err => console.error('showCountries got an error: ' + err)
+    )
+  }
+
+  back() {
+    this.selected = null;
+    this.router.navigateByUrl('countries')
+  }
+
+
+
+  cancel() {
+    this.editCountry = null;
+  }
+
+
+  private loginCheck() {
+    if (!this.authService.checkLogin) {
+      this.router.navigateByUrl('/home');
+    }
+  }
+
+  editComment(comment: Comment, i) {
+    // this.commentEdit = comment;
+    // this.commentIndex = i;
+
+  }
+
+  editResponse(comment: Comment, i) {
+    // this.responseEdit = comment;
+    // this.responseIndex = i;
+  }
+
+  saveEdit(comment: Comment) {
+    let cid = +this.route.snapshot.paramMap.get('cid');
+    this.commentServ.update(comment, cid).subscribe(
+      data => {
+        this.showCountry(cid);
+        // this.commentEdit=null;
+        // this.responseEdit=null;
+      },
+      fail => {
+        console.error('CountryListComponent.editComment() failed:');
+        console.error(fail);
+      }
+    )
+  }
+
+  saveNewComment(comment: Comment) {
+    let cid = +this.route.snapshot.paramMap.get('cid');
+    this.commentServ.create(cid, comment).subscribe(
+      data => {
+        this.showCountry(cid);
+        this.createComment = new Comment();
+      },
+      fail => {
+        console.error('CountryListComponent.editComment() failed:');
+        console.error(fail);
+      }
+    )
+  }
+
+  deleteComment(id: number) {
+    console.log(id);
+
+    let cid = +this.route.snapshot.paramMap.get('cid');
+    this.commentServ.delete(cid, id).subscribe(
+      data => {
+        this.showCountry(cid);
+      },
+      fail => {
+        console.error('CountryListComponent.deleteComment() failed:');
+        console.error(fail);
+      }
+    )
   }
 }
 
