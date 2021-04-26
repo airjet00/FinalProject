3c5bcc6f574ac587a179b309697ec0906736096f
Your Name <you@example.com>
2021-04-26 16:04:04 -0600

    completed ability to add countries to wishlist

--- ngVoyagerHB/src/app/components/register/register.component.ts
+++ ngVoyagerHB/src/app/components/register/register.component.ts
@@ -4,6 +4,7 @@ import { Trip } from 'src/app/models/trip';
 import { User } from 'src/app/models/user';
 import { AuthService } from 'src/app/services/auth.service';
 import { TripService } from 'src/app/services/trip.service';
+import { UserService } from 'src/app/services/user.service';
 
 @Component({
   selector: 'app-register',
@@ -18,7 +19,8 @@ export class RegisterComponent implements OnInit {
     private authSvc: AuthService,
     private route: ActivatedRoute,
     private router: Router,
-    private tripSvc: TripService
+    private tripSvc: TripService,
+    private userSvc: UserService
   ) { }
 
   ngOnInit(): void {
@@ -29,15 +31,22 @@ export class RegisterComponent implements OnInit {
       returnedUser => {
         this.authSvc.login(this.user.username, this.user.password).subscribe(
           data => {
+
             let wishlist = new Trip();
             wishlist.name = "Wishlist";
             wishlist.startDate = "2021-09-01T00:00:00";
             wishlist.endDate = "2021-09-01T00:00:00";
             wishlist.completed = false;
-            this.tripSvc.create(wishlist);
-            console.log(wishlist);
+            wishlist.enabled = true;
 
-            this.router.navigateByUrl("countries")
+            this.createWL(wishlist);
+
+            this.getUserInfo(data['name']);
+
+            if(localStorage.getItem("username")){
+              this.router.navigateByUrl("countries")
+            }
+
           },
           err => {
             console.error("Encountered Error logging in: " + err);
@@ -52,6 +61,42 @@ export class RegisterComponent implements OnInit {
 
       }
     )
+  }
+
+  createWL(wishlist: Trip): void {
+    this.tripSvc.create(wishlist).subscribe(
+      data => {},
+      err => {
+        console.error("Observer encountered error: " + err);
+      }
+    );
+  }
+
+  getUserInfo(username: string): void {
+    let user: User = null;
+    this.userSvc.showByUsername(username).subscribe(
+      data => {
+        user = data;
+        localStorage.setItem('userRole', user.role);
+        localStorage.setItem('userFirstName', user.firstName);
+        localStorage.setItem('userLastName', user.lastName);
+        localStorage.setItem('username', user.username);
+
+        if (localStorage.getItem('userRole') === "admin") {
+          this.router.navigateByUrl("admin-dashboard");
+          console.warn("if");
+          }
+
+        else {
+          this.router.navigateByUrl("countries");
+          console.warn("else");
+
+        }
+      },
+      err => {
+        console.error("Observer encountered error: " + err);
+      }
+    );
   }
 
 }
