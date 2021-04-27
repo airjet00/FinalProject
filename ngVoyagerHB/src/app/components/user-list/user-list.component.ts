101906c1a121641e6c918492d3adf3b7285aa64e
erikskell <erikskell@gmail.com>
2021-04-27 12:05:15 -0500

    Added comments and featured countries to admin dashboard

--- ngVoyagerHB/src/app/components/user-list/user-list.component.ts
+++ ngVoyagerHB/src/app/components/user-list/user-list.component.ts
@@ -1,88 +1,90 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 import { ActivatedRoute, Router } from '@angular/router';
 import { User } from 'src/app/models/user';
+import { Comment } from 'src/app/models/comment';
 import { AuthService } from 'src/app/services/auth.service';
+import { CommentService } from 'src/app/services/comment.service';
 import { UserService } from 'src/app/services/user.service';
 
 @Component({
   selector: 'app-user-list',
   templateUrl: './user-list.component.html',
-  styleUrls: ['./user-list.component.css']
+  styleUrls: ['./user-list.component.css'],
 })
 export class UserListComponent implements OnInit {
-
   users: User[] = [];
   newUser: User = new User();
   usersname: string = null;
   selected: User = null;
-
+  commentsByUser: Comment[];
 
   constructor(
     private userServ: UserService,
     private router: Router,
     private authService: AuthService,
-    private route: ActivatedRoute
-  ) { }
+    private route: ActivatedRoute,
+    private commServ: CommentService
+  ) {}
 
   ngOnInit(): void {
-
-      this.loadUsers();
-
-
-
-
+    this.loadUsers();
+    this.users.forEach(u => {
+      this.filterByUser(u);
+    });
   }
 
-
-// Load all users for Admin only
-  loadUsers(){
+  // Load all users for Admin only
+  loadUsers() {
     this.userServ.index().subscribe(
-      data => {
+      (data) => {
         this.users = data;
       },
-      fail => {
-        console.error("Error loading users " + fail);
+      (fail) => {
+        console.error('Error loading users ' + fail);
         console.error(fail);
-      });
+      }
+    );
   }
 
-  loadUser(id){
+  loadUser(id) {
     this.userServ.show(id).subscribe(
-      data => {
+      (data) => {
         this.selected = data;
       },
-      fail => {
-        console.error("Error loading user " + fail);
+      (fail) => {
+        console.error('Error loading user ' + fail);
         console.error(fail);
-      });
+      }
+    );
   }
 
   updateUser(editUser: User): void {
-    this.userServ.update(editUser).subscribe(
-      data => {
-
+    this.newUser.id = editUser.id;
+    this.userServ.update(this.newUser).subscribe(
+      (data) => {
         this.loadUser(data.id);
       },
-      fail => {
-        console.error("Error updating user " + fail);
+      (fail) => {
+        console.error('Error updating user ' + fail);
         console.error(fail);
-      });
+      }
+    );
   }
 
   // Flip user enable to false to disable account
-  deleteUser(id){
+  deleteUser(id) {
     this.userServ.delete(id).subscribe(
-      data => {
+      (data) => {
         this.loadUsers();
       },
-      fail => {
-        console.error("Error deleting user " + fail);
+      (fail) => {
+        console.error('Error deleting user ' + fail);
         console.error(fail);
-      });
+      }
+    );
   }
 
   enableUser(user) {
-
     let sendUser = new User();
     sendUser.id = user.id;
     sendUser.email = user.email;
@@ -94,13 +96,29 @@ export class UserListComponent implements OnInit {
     sendUser.enabled = true;
     sendUser.role = user.role;
     this.userServ.update(sendUser).subscribe(
-      data => {
+      (data) => {
         this.loadUsers();
       },
-      fail => {
-        console.error("Error enabling user " + fail);
-      });
+      (fail) => {
+        console.error('Error enabling user ' + fail);
+      }
+    );
   }
 
+
+indexByUsername(username: string) {
+  this.commServ.indexByUsername(username).subscribe(
+    data => {
+      return data;
+    },
+    failure => {
+      console.error(failure);
+    });
+  return null;
+}
+
+filterByUser(user: User){
+
+}
 
 }
